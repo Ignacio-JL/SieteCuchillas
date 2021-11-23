7ec293e816b4f0a98256307a78a565dd38c8c6a6
IgnacioPC <joseignacio439@gmail.com>
2021-11-23 19:45:31 -0300

    validacion de formulario

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 document.getElementById('enviarForm').addEventListener('click', (e) =>{
-    e.preventDefault();
-    window.location.href=`mailto:prueba@gmail.com?&body=
-    Nombre%3A%20${document.getElementById('nombre').value}%0A
-    Correo%3A%20${document.getElementById('correo').value}%0A
-    Teléfono%3A%20${document.getElementById('telefono').value}%0A
-    Mensaje%3A%20${document.getElementById('mensaje').value}%0A`
+    const nombre = document.getElementById('nombre').value;
+    const correo = document.getElementById('correo').value;
+    const telefono = document.getElementById('telefono').value;
+    const mensaje = document.getElementById('mensaje').value;
+    
+    if(nombre.length != 0 && correo.length != 0 && telefono.length != 0 && mensaje.length != 0){
+        e.preventDefault();
+        window.location.href=`mailto:prueba@gmail.com?&body=
+        Nombre%3A%20${nombre}%0A
+        Correo%3A%20${correo}%0A
+        Teléfono%3A%20${telefono}%0A
+        Mensaje%3A%20${mensaje}%0A`
+    }
+    
 });

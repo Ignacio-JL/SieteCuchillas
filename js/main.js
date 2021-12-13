e18996e5e8bb6b5f463066829f896ca69e739aa4
IgnacioPC <joseignacio439@gmail.com>
2021-12-13 16:11:14 -0300

    prev to merge

--- js/main.js
+++ js/main.js
@@ -6,24 +6,6 @@
         }, 800);
     });
 
-//Formulario
-//(Descartado) -- Borrar
-// document.getElementById('enviarForm').addEventListener('click', (e) =>{
-    // const nombre = document.getElementById('nombre').value;
-    // const correo = document.getElementById('correo').value;
-    // const telefono = document.getElementById('telefono').value;
-    // const mensaje = document.getElementById('mensaje').value;
-    
-    // if(nombre.length != 0 && correo.length != 0 && telefono.length != 0 && mensaje.length != 0){
-//         e.preventDefault();
-//         window.location.href=`mailto:prueba@gmail.com?&body=
-//         Nombre%3A%20${nombre}%0A
-//         Correo%3A%20${correo}%0A
-//         Teléfono%3A%20${telefono}%0A
-//         Mensaje%3A%20${mensaje}%0A`
-//     }
-
-// });
 
 //Control campos vacios y check del formato de correo
 function controlFormulario(){
@@ -34,13 +16,7 @@ function controlFormulario(){
             return false;
         }
 }
-//Reinicio campos de formulario sin tener que validarlos
-function vaciarInputs(){
-    $('#nombre').val('');
-    $('#correo').val('');
-    $('#telefono').val('');
-    $('#mensaje').val('');
-}
+
 
 //Confirmacion Mail
 function mailOk (){
@@ -153,8 +129,6 @@ const mostrarProductos = (stockProductos) => {
 }
 
 
-
-//////////////////
 
 /*
 

0ac3adbdacf30246e1274f7e4ea0615504b257f0
IgnacioPC <joseignacio439@gmail.com>
2021-12-23 17:33:59 -0300

    proyecto final

--- js/main.js
+++ js/main.js
@@ -1,63 +1,59 @@
+
 //Scroll Contacto
-    $('#itemContacto').on('click',function(){
-        $('#navbarTogglerDemo2').removeClass('show');
-        $('html, body').animate({
-            scrollTop: $('#contacto').offset().top
-        }, 800);
-    });
+$('#itemContacto').on('click',function(){
+    $('#navbarTogglerDemo2').removeClass('show');
+    $('html, body').animate({
+        scrollTop: $('#contacto').offset().top
+    }, 800);
+});
 
 
 //Control campos vacios y check del formato de correo
 function controlFormulario(){
-        if($('#nombre').val().length != 0 && $('#correo').val().length != 0 && $('#telefono').val().length != 0 && $('#mensaje').val().length != 0 && $('#checkCorreo').css('display') == 'none'){
-            return true;
-        }
-        else{
-            return false;
-        }
+    if($('#nombre').val().length != 0 && $('#correo').val().length != 0 && $('#telefono').val().length != 0 && $('#mensaje').val().length != 0 && $('#checkCorreo').css('display') == 'none'){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 //Confirmacion Mail
 function mailOk (){
-    $('.mailOk').toggle(0).animate({
-        opacity: '1'
-        }, "slow"
-    )
-    .delay(3000)
-    .animate({
-        opacity: '1'
-    }, 0,
-    function (){location.reload();}
-    );
+$(".footer").css('z-index', '1 !important');
+$('.mailOk').toggle(0).animate({
+    opacity: '1'
+    }, "slow"
+)
+.delay(3000)
+.animate({
+    opacity: '1'
+}, 0,
+function (){window.location.reload();}
+);
 }
 
-//Envio de mail
+//Envio Mail fetch
 async function handleSubmit(event){
-    event.preventDefault();
-    if(controlFormulario()){
+event.preventDefault();
+if(controlFormulario()){
 
-        const miForm = new FormData(this);
-        const response = await fetch(this.action, {
-            method: this.method,
-            body: miForm,
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-        if(response.ok){
-            
-            mailOk();
+    const miForm = new FormData(this);
+    const response = await fetch("https://formsubmit.co/contacto@sietecuchillas.com.ar", {
+        method: "POST",
+        body: miForm,
+        headers: {
+            'Accept': 'application/json'
         }
+    });
+    if(response.ok){
+        mailOk();
     }
 }
+}
+
+
 
 const form = document.querySelector('.formulario');
 form.addEventListener('submit', handleSubmit);
-
-
-
-
-
-
-
 
